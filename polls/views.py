8e5c971d4721dd5c3c27012963b00d98886bec9c
Gui69 <guilherme.melo@escolar.ifrn.edu.br>
2022-08-26 16:51:22 -0300

    3° e 4° Tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,46 @@
+from audioop import reverse
+from webbrowser import get
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
+from django.urls import reverse
+
+
+
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def index(request):
-    return HttpResponse("Olá, mundo. Guilherme é lindo.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,}
+    return HttpResponse(template.render(context, request))
+
+def results(request, question_id):
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk =question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    
+    else:
+        selected_choice.votes += 1 
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+
